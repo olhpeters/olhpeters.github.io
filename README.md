d333de7915ccda9536b9ff7d9a222f356c537e22
Oliver Peters <olhpeters@gmail.com>
2023-07-08 20:15:17 -0400

    implemented print table and download buttons

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # olhpeters.github.io
 Oliver public site
+
+## Summary
+Talk to table is a web application that allows users to upload a data table (CSV and Excel formats) and lets you talk to an AI using natural language which modifies and recieves data such as statistics. The intent of this tool is to simplify routine data science tasks using a minimal and intuitive interface. 
--- js/upload.js
+++ js/upload.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
 
+  var table
+
   function buildTable(tabledata) {
-    var table = new Tabulator("#example-table", {
+    table = new Tabulator("#example-table", {
       data: tabledata, //assign data to table
       layout: "fitData", //fit columns to width of table (optional)
       autoColumns:true,
+      printAsHtml:true, 
       pagination:"local",
       paginationSize:6,
       paginationSizeSelector:[3, 6, 8, 10, 50],
@@ -19,7 +22,7 @@ $(document).ready(function () {
     { id: 3, name: "Christine Lobowski", age: "42", col: "green", dob: "22/05/1982" },
     { id: 4, name: "Brendon Philips", age: "125", col: "orange", dob: "01/08/1980" },
     { id: 5, name: "Margret Marmajuke", age: "16", col: "yellow", dob: "31/01/1999" },
-  ];
+  ]
 
   buildTable(tabledata)
 
@@ -41,7 +44,47 @@ $(document).ready(function () {
         console.log(tableData)
         buildTable(tableData)
       }
-    });
-  });
+    })
+  })
 
-});
+  $('#print-table').click(function (e) {
+    table.print(false, true)
+  })
+
+  $('#download-csv').click(function (e) {
+    table.download("csv", "data.csv");
+  })
+
+  $('#download-xlsx').click(function (e) {
+    table.download("xlsx", "data.xlsx", {sheetName:"My Data"});
+  })
+
+  $('#download-pdf').click(function (e) {
+    table.download("pdf", "data.pdf", {
+      orientation:"portrait", //set page orientation to portrait
+      title:"Talked to table", //add title to report
+    });
+  })
+
+  $('#talk-button').click(function (e) {
+    e.preventDefault();
+    var talkInput=$("#talkInput").val()
+    console.log(talkInput)
+    $.ajax({
+      url: "http://127.0.0.1:8000/chat",
+      type: 'POST',
+      data: JSON.stringify({
+        talk_input: talkInput,
+      }),
+      contentType: 'application/json',
+      dataType: "json",
+      success: function (response) {
+        $('#talk-response').html(response.message)
+        /*tableData = JSON.parse(response.table)
+        console.log(tableData)*/
+        console.log(response)
+        //buildTable(tableData)
+      }
+    })
+  })
+})
